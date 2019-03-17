9951e8deef0409da67ab5d71a41502aff859d639
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-16 17:30:14 -0700

    Import programs data to programs page

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,16 +1,41 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link, graphql } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
-const ProgramsPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the programs page</h1>
-    <p>This is where programs will live</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const ProgramsPage = ({ data }) => {
+  const { programs } = data.allDataJson.edges[0].node;
+  console.log(programs);
+  return (
+    <Layout>
+      <SEO title="Page two" />
+      <h1>Hi from the programs page</h1>
+      <p>This is where programs will live</p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  )
+}
 
 export default ProgramsPage;
+
+export const query = graphql`
+{
+  allDataJson {
+    edges {
+      node {
+        programs {
+          _1 {
+            name
+            description
+          }
+          _2 {
+            name
+            description
+          }
+        }
+      }
+    }
+  }
+}
+`
