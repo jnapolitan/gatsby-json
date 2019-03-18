2c545a7f8edaf8acb2a7d2443d145c5b381dc8e5
Julian Napolitan <julian.napolitan@gmail.com>
2019-03-18 13:26:32 -0700

    Create Section nodes and slugs for programmatic page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,38 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const crypto = require('crypto');
+const { createFilePath } = require(`gatsby-source-filesystem`);
 
-// You can delete this file if you're not using it
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNode, createNodeField } = actions;
+  if (node.internal.type === `ProgramsJson`) {
+    node.sections.forEach((section, idx) => {
+      createNode({
+        id: `${node.id}-${idx}`,
+        programId: `${node.id}`,
+        name: section.name,
+        description: section.description,
+        image: section.image,
+        order: section.order,
+        activities: section.activities,
+        parent: `${node.id}`,
+        children: [],
+        internal: {
+          type: `Section`,
+          contentDigest: crypto
+            .createHash(`md5`)
+            .update(JSON.stringify(section))
+            .digest(`hex`),
+          mediaType: `text/markdown`,
+        }
+      });
+    });
+  }
+
+  if (node.internal.type === 'Section') {
+    const slug = `${node.programId}/${node.order + 1}`;
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
